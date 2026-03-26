3b5b3f1d6d8cec5fe8949b3bd129d1c072c1fa99
Trada Kevin <tradakevin0099@gmail.com>
2026-03-26 16:25:07 +0530

    Add Simulation Button in game and delete some useless script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    public static event Action OnSimulationStart;
+    public static event Action OnSimulationStop;
+
+
+
+    [SerializeField] private Button simulationButton;
+    [SerializeField] private TextMeshProUGUI simulationButtonText;
+
+    
+
+    private bool isSimulating = false;
+
+    private void Awake()
     {
-        
+        if(instance == null)
+            instance = this;
+        else
+            DontDestroyOnLoad(instance);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleSimulation()
     {
-        
+        isSimulating = !isSimulating;
+
+        simulationButtonText.text = isSimulating ? "Stop" : "Simulate";
+
+        if (isSimulating)
+        {
+            OnSimulationStart?.Invoke();
+            Debug.Log(OnSimulationStart?.GetInvocationList().Length ?? 0);
+        }
+        else
+        {
+            OnSimulationStop?.Invoke();
+        }
     }
+
 }
